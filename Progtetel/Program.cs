9940e02c714db10cf07cccb906c67df9087e1737
Fodor Márk Pál <fodor.mark@ckik.loc>
2024-12-04 11:03:12 +0100

    X tömb

--- Progtetel/Program.cs
+++ Progtetel/Program.cs
@@ -6,7 +6,14 @@ namespace Progtetel
     {
         static void Main(string[] args)
         {
+            int[] x = { 2, 3, 7, 4, 8, -5, 6 };
 
+            //Párosak kiválogatása - 1
+            
+            
+
+            //Maximum kiválasztás - 2
+            
         }
     }
 }
