43e1d77f015660b82e8d5b8089e973c41b2b3267
fulep.fanni <fulep.fanni@ckik.hu>
2024-12-04 11:17:06 +0100

    Párosak kiválogatása

--- Progtetel/Program.cs
+++ Progtetel/Program.cs
@@ -6,14 +6,36 @@ namespace Progtetel
     {
         static void Main(string[] args)
         {
-            int[] x = { 2, 3, 7, 4, 8, -5, 6 };
+            int[] x = { 2, 3, 7, 4, 8, -5, 6};
 
             //Párosak kiválogatása - 1
-            
-            
+
+            int[] parosak = new int[100];
+
+            int i;
+            int db = 0;
+
+            for (i = 0; i < x.Length; i++)
+            {
+
+                if (x[i] % 2 == 0)
+                {
+                    parosak[db] = x[i];
+                    db++;
+                }
+            }
+
+            for (i = 0; i < db; i++)
+            {
+                Console.Write(parosak[i] + " ");
+            }
+            Console.WriteLine();
+
+
 
             //Maximum kiválasztás - 2
-            
+
+            Console.ReadKey();
         }
     }
 }
